a61edf5a87eb8b8b1eea202186c0ed62c9067efc
Ceylon500 <e87334745@gmail.com>
2024-12-11 20:15:25 +0300

    Add files via upload
    Домашнее задание "Организация программ и методы строк"

---
+++ task4.py
@@ -0,0 +1,11 @@
+# ввод и вывод переменной с подсчетом количества символов в заданной переменной
+my_string=input('Моя первая строка: ')
+print('Результат: ', my_string)
+print(len(my_string))
+# Работа с методами строк
+my_string=input('Ввод в консоль: ')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
